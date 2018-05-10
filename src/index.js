0aa16b83224191332acb9df74d13380fb7e166e5
UnnamedHero <hero.nospam@gmail.com>
2018-05-10 13:18:14 +0300

    make vendor and app ks files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
-
-// import faker from 'faker';
-import gon from 'gon';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
-
-import ReactDOM from 'react-dom';
-import React from 'react';
-import ChannelsList from '../app/components/ChannelsList.jsx';
-
-
-if (process.env.NODE_ENV !== 'production') {
-  localStorage.debug = 'chat:*';
-}
-
-ReactDOM.render(<ChannelsList gon={window.gon} />, document.getElementById('chat'));
