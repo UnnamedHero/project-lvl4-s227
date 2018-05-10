cc8f6105791e4e1c9de59c834f586589e3c7e494
UnnamedHero <hero.nospam@gmail.com>
2018-05-10 17:54:06 +0300

    app init logic

--- app/index.js
+++ app/index.js
@@ -1,15 +1,41 @@
-// import faker from 'faker';
-import gon from 'gon';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
 
+// import io from 'socket.io-client';
+// import gon from 'gon';
+import Cookie from 'js-cookie';
+import faker from 'faker';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import ChannelsList from '../app/components/ChannelsList.jsx';
+import { Provider } from 'react-redux';
+import  { createStore, applyMiddleware, compose } from 'redux';
+import App from '../app/components/App';
+import reducers from './reducers';
 
+const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
+const devtoolMiddleware = ext && ext();
+
+const store = createStore(
+  reducers,
+  compose(    
+    devtoolMiddleware,    
+  ),
+);
+
+const initUserName = () => {
+  const cookieName = Cookie.get('userName');  
+  if (!cookieName) {
+    Cookie.set('userName', faker.name.findName());
+  }
+};
+
+initUserName();
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-ReactDOM.render(<ChannelsList gon={window.gon} />, document.getElementById('chat'));
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+ document.getElementById('chat')
+);
