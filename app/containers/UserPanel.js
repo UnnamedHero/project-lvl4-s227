80a4f7e40bf95481982f6a1af7a2a65cd7adc124
UnnamedHero <hero.nospam@gmail.com>
2018-05-11 14:48:08 +0300

    add simple userpanel

---
+++ app/containers/UserPanel.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import Cookie from 'js-cookie';
+import Component from '../components/UserPanel';
+
+const Container = connect(
+  (state) => {
+    const props = {
+      userName: Cookie.get('userName'),    
+    };
+    return props;
+  },  
+)(Component);
+
+export default Container;
