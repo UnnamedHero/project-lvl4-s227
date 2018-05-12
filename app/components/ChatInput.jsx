a31770f04dd4983d6a87ff65baab785281947a18
UnnamedHero <hero.nospam@gmail.com>
2018-05-12 19:34:27 +0300

    fix eslint error

--- app/components/ChatInput.jsx
+++ app/components/ChatInput.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export default class ChatInput extends React.Component {
-  render() {
-    return (
-    <form>
-    <div className='form-group'>
-      <input type='text' className='form-control' />
+const ChannelInput = () => (
+  <form>
+    <div className="form-group">
+      <input type="text" className="form-control" />
     </div>
   </form>);
-  }
-};
+
+export default ChannelInput;
