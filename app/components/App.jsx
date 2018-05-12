1a33eaf20e3cccd95fca056531ad219fa2284552
UnnamedHero <hero.nospam@gmail.com>
2018-05-12 19:34:16 +0300

    fix eslint error

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,31 +5,31 @@ import ChatWindow from './ChatWindow';
 import ChatInput from './ChatInput';
 
 const App = () => (
-  <div className='container-fluid d-flex vh-100'>
-    <div className = 'row flex-fill'>
-      <div className='col-3 flex-fill scrollable'>  
-        <div className='row'>
-          <div className='col-12'>
+  <div className="container-fluid d-flex vh-100">
+    <div className="row flex-fill">
+      <div className="col-3 flex-fill scrollable">
+        <div className="row">
+          <div className="col-12">
             <UserPanelContainer />
           </div>
         </div>
-        <div className='row'>    
-          <div className='col-12'>
-            <ChannelsListContainer />     
-          </div>            
+        <div className="row">
+          <div className="col-12">
+            <ChannelsListContainer />
+          </div>
         </div>
       </div>
-      <div className='col-9 vh-100'>
-        <div className='row h-75'>
-          <div className='col-12 scrollable'>
+      <div className="col-9 vh-100">
+        <div className="row h-75">
+          <div className="col-12 scrollable">
             <ChatWindow />
           </div>
         </div>
-        <div className='row h-25'>
-          <div className='col-12'>
+        <div className="row h-25">
+          <div className="col-12">
             <ChatInput />
           </div>
-        </div>  
+        </div>
       </div>
     </div>
   </div>
