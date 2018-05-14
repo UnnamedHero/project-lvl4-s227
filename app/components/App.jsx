d04ac4133743c32efde0a9141b69248345c359e3
UnnamedHero <hero.nospam@gmail.com>
2018-05-14 15:24:12 +0300

    change component  grid

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 import ChannelsListContainer from '../containers/ChannelsList';
-import UserPanelContainer from '../containers/UserPanel';
+// import UserPanelContainer from '../containers/UserPanel';
 import ChatWindow from './ChatWindow';
 import ChatInput from './ChatInput';
 
 const App = () => (
-  <div className="container-fluid d-flex vh-100">
+  <div className="container-fluid">
     <div className="row flex-fill">
-      <div className="col-3 flex-fill scrollable">
-        <div className="row">
-          <div className="col-12">
-            <UserPanelContainer />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <ChannelsListContainer />
-          </div>
-        </div>
+      <div className="col-2">
+        <ChannelsListContainer />
       </div>
-      <div className="col-9 vh-100">
-        <div className="row h-75">
-          <div className="col-12 scrollable">
+      <div className="col-10">
+        <div className="row">
+          <div className="col">
             <ChatWindow />
           </div>
         </div>
-        <div className="row h-25">
-          <div className="col-12">
+        <div className="row">
+          <div className="col">
             <ChatInput />
           </div>
         </div>
