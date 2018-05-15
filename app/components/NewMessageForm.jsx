ce039d4a7b96ef0960910b15b701c239ad55965d
UnnamedHero <hero.nospam@gmail.com>
2018-05-15 18:24:46 +0300

    newMessageForm reducer

---
+++ app/components/NewMessageForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class NewMessageForm extends React.Component {
+  sendMessage = (values) => {
+    console.log(values);
+    this.props.reset();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.sendMessage)}>
+        <Field name="message" component="input" required type="text" className="form-control" />
+        <button type="submit" className="btn btn-primary">Send</button>
+      </form>);
+  }
+}
+
+export default reduxForm({
+  form: 'newMessage',
+})(NewMessageForm);
