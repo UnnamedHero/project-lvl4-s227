7698e666a22d93148b30d0006d263b4c7146e5f7
UnnamedHero <hero.nospam@gmail.com>
2018-05-17 10:57:11 +0300

    remove initmessages action

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,7 +2,6 @@ import { createAction } from 'redux-actions';
 import axios from 'axios';
 
 export const initUserName = createAction('USER_INIT_NAME');
-export const initChannelsList = createAction('CHANNELS_INIT_LIST');
 
 export const sendMessageRequest = createAction('MESSAGE_SEND_REQUEST');
 export const sendMessageSuccess = createAction('MESSAGE_SEND_SUCCESS');
