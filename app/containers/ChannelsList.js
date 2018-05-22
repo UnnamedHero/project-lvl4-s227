19b56d5b617e104a506cfa149f1a9fe64ca92f36
UnnamedHero <hero.nospam@gmail.com>
2018-05-22 14:24:12 +0300

    add action to channelList props

--- app/containers/ChannelsList.js
+++ app/containers/ChannelsList.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import Component from '../components/ChannelsList';
+import * as actionCreators from '../actions';
 
 const mapStateToProps = ({ channelsList: { channels, currentChannelId } }) => {
   const props = {
@@ -8,6 +9,6 @@ const mapStateToProps = ({ channelsList: { channels, currentChannelId } }) => {
   return props;
 };
 
-const Container = connect(mapStateToProps)(Component);
+const Container = connect(mapStateToProps, actionCreators)(Component);
 
 export default Container;
