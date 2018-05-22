85d04a0ac98323025594b111264b3e5ee920bda0
UnnamedHero <hero.nospam@gmail.com>
2018-05-22 17:59:35 +0300

    add channels edit button

--- app/components/ChannelsList.jsx
+++ app/components/ChannelsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 import cn from 'classnames';
+import ChannelsListEditor from './ChannelsListEditor';
 import connect from '../connect';
 
 const mapStateToProps = ({ channelsList: { channels, currentChannelId } }) => {
@@ -10,9 +11,16 @@ const mapStateToProps = ({ channelsList: { channels, currentChannelId } }) => {
   return props;
 };
 
-const ChannelsList = (props) => {
-  const renderChannelsList = () => {
-    const { channels, currentChannelId, changeCurrentChannel } = props;
+@connect(mapStateToProps)
+class ChannelsList extends React.Component {
+  state = { isEditorOpen: false }
+
+  toggleEditor = () => {
+    this.setState({ isEditorOpen: !this.state.isEditorOpen });
+  }
+
+  renderChannelsList = () => {
+    const { channels, currentChannelId, changeCurrentChannel } = this.props;
     return channels.map((channel) => {
       const isCurrentChannel = currentChannelId === channel.id;
       const itemClass = {
@@ -28,15 +36,22 @@ const ChannelsList = (props) => {
           {channel.name}
         </ListGroupItem>);
     });
-  };
-  return (
-    <div>
-      <span>Channels:</span>
-      <ListGroup>
-        {renderChannelsList()}
-      </ListGroup>
-    </div>
-  );
-};
+  }
 
-export default connect(mapStateToProps)(ChannelsList);
+  render() {
+    return (
+      <div>
+        <span>
+          Channels:
+          <Button color="link" onClick={this.toggleEditor}>edit</Button>
+          <ChannelsListEditor isOpen={this.state.isEditorOpen} toggle={this.toggleEditor} />
+        </span>
+        <ListGroup>
+          {this.renderChannelsList()}
+        </ListGroup>
+      </div>
+    );
+  }
+}
+
+export default ChannelsList;
