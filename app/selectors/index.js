33f8b8e5fc1da198220e1102f8e6ef11d3631d86
UnnamedHero <hero.nospam@gmail.com>
2018-05-24 17:44:11 +0300

    remove useless editable selector

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -3,15 +3,8 @@ import { createSelector } from 'reselect';
 export const getMessages = state => state.messages;
 export const getCurrentChannel = state => state.channelsList.currentChannelId;
 
-export const getChannels = state => state.channelsList.channels;
-
 export const getCurrentChannelMessagesSelector = createSelector(
   getMessages,
   getCurrentChannel,
   (messages, currentChannelId) => messages.filter(m => m.channelId === currentChannelId),
-);
-
-export const getEditableChannels = createSelector(
-  getChannels,
-  channels => channels.filter(c => c.removable),
 );
