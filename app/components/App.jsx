478f45024a9a7638b3b9d2ccab3053a02ad06aca
UnnamedHero <hero.nospam@gmail.com>
2018-05-25 16:55:30 +0300

    Move channelList header to a separate component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ChannelsListHeader from './ChannelsListHeader';
 import ChannelsList from './ChannelsList';
 import UserPanel from './UserPanel';
 import ChatWindow from './ChatWindow';
@@ -8,15 +9,18 @@ import AlertNotifier from './AlertNotifier';
 const App = () => (
   <div className="container-fluid">
     <div className="row d-flex vh-100">
-      <div className="col-2 d-flex flex-column bg-dark text-white-50 vh-100">
+      <div className="col-3 d-flex flex-column bg-dark text-white-50 vh-100">
         <div>
           <UserPanel />
+        </div>
+        <div>
+          <ChannelsListHeader />
         </div>
         <div className="scrollable">
           <ChannelsList />
         </div>
       </div>
-      <div className="d-flex col-10 flex-column justify-content-between vh-100 bg-secondary">
+      <div className="d-flex col-9 flex-column justify-content-between vh-100 bg-secondary">
         <div className="scrollable word-wrap-bw">
           <ChatWindow />
         </div>
