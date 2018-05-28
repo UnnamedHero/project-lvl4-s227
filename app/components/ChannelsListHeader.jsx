fb98fe8f379ee9b2c4c49a41acdcfddedf7f4eab
UnnamedHero <hero.nospam@gmail.com>
2018-05-28 14:29:59 +0300

    move add channel to a ChannelsListHeader

--- app/components/ChannelsListHeader.jsx
+++ app/components/ChannelsListHeader.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import ChannelsListEditor from './ChannelsListEditor';
+import ModalEditor from './ModalEditor';
+import connect from '../connect';
+import validateChannelName from '../formValidators';
 
+
+const mapStateToProps = (state) => {
+  const props = {
+    uiEditChannels: state.UI.editChannels,
+    channels: state.channels.channelsList,
+  };
+  return props;
+};
+
+@connect(mapStateToProps)
 class ChannelsListHeader extends React.Component {
-  state = { isEditorOpen: false }
+  state = { isModalOpen: false }
+
+  addChannel = ({ modalEditorInput: name }) => {
+    this.props.addChannel(name);
+  }
 
   toggleEditor = () => {
-    this.setState({ isEditorOpen: !this.state.isEditorOpen });
+    this.props.toggleEditChannelsUiState();
+    // this.setState({ isEditorOpen: !this.state.isEditorOpen });
+  }
+
+  toggleAddChannelModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+
+  renderAddChannelModal = () => {
+    const addProps = {
+      headerLabel: 'Add channel',
+      submitLabel: 'Add',
+      cancelLabel: 'Close',
+      submitHandler: this.addChannel,
+      cancelHandler: this.toggleAddChannelModal,
+      validate: validateChannelName.bind(null, this.props.channels),
+      enableReinitialize: true,
+      initialValues: {},
+      requestType: 'channelAddState',
+      closeOnSuccess: false,
+    };
+    return <ModalEditor isOpen {...addProps} />;
   }
 
   render() {
@@ -14,7 +51,8 @@ class ChannelsListHeader extends React.Component {
       <span>
         Channels:
         <Button color="link" onClick={this.toggleEditor}>edit</Button>
-        <ChannelsListEditor isOpen={this.state.isEditorOpen} toggle={this.toggleEditor} />
+        { this.props.uiEditChannels && <Button color="link" onClick={this.toggleAddChannelModal}>Add channel</Button> }
+        { this.state.isModalOpen && this.renderAddChannelModal() }
       </span>
     );
   }
