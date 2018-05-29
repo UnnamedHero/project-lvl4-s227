9866cddc2fbdcce36483f883cd8952a648f8051d
UnnamedHero <hero.nospam@gmail.com>
2018-05-29 14:14:32 +0300

    no empty names

--- app/formValidators.js
+++ app/formValidators.js
@@ -1,8 +1,15 @@
 import find from 'lodash/find';
 
-const validateChannelName = (channels, name) => {
-  const isExists = find(channels, ch => ch.name === name);
-  return isExists ? { error: 'Channel name already exist' } : null;
+const validateChannelName = (channels, name = '') => {
+  const trimmedName = name.toString().trim();
+  const isExists = find(channels, ch => ch.name === trimmedName);
+  if (isExists) {
+    return { error: 'Channel name already exist' };
+  }
+  if (trimmedName.length === 0) {
+    return { error: 'Channel name cannot be blank' };
+  }
+  return null;
 };
 
 export default validateChannelName;
