dde00c0ef15959d2f12fd499a9090da7fd7e6080
UnnamedHero <hero.nospam@gmail.com>
2018-06-04 11:38:24 +0300

    init compomemt props mapping

--- app/components/ChannelsPanel.jsx
+++ app/components/ChannelsPanel.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import ChannelsPanelHeader from './ChannelsPanelHeader';
 import ChannelsList from './ChannelsList';
+import connect from '../connect';
+
+const mapStateToProps = (state) => {
+  const props = {
+    channelsList: state.channels.channelsList,
+    defaultChannelId: state.channels.defaultChannelId,
+    currentChannelId: state.channels.currentChannelId,
+  };
+  return props;
+};
 
 class ChannelsPanel extends React.Component {
   render() {
