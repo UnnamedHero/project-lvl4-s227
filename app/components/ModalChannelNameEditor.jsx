5c09b4d80a58b2cfce04170ec61c8c573485d3c6
UnnamedHero <hero.nospam@gmail.com>
2018-06-05 13:55:30 +0300

    refactor and rename modal editor.

---
+++ app/components/ModalChannelNameEditor.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
+
+import InputField from './InputField';
+
+@reduxForm({
+  form: 'ModalEditor',
+})
+class ModalChannelNameEditor extends React.Component {
+  static defaultProps = {
+    isValidInput: true,
+  };
+  // static getDerivedStateFromProps(nextProps) {
+  //   return { errorPopover: has(nextProps.inputState, 'ModalEditor.syncErrors') };
+  // }
+
+  // state = { errorPopover: false };
+
+  // componentDidUpdate(prevProps) {
+  //   const { requestType, closeOnSuccess, cancelHandler } = this.props;
+  //   const prevState = prevProps.requestStates[requestType];
+  //   const currState = this.props.requestStates[requestType];
+  //   if (prevState !== currState && currState !== 'requested') {
+  //     this.props.reset();
+  //     if (closeOnSuccess) {
+  //       cancelHandler();
+  //     }
+  //   }
+  // }
+
+  render() {
+    const {
+      // isOpen,
+      // headerLabel,
+      handleToggleModal,
+      handleSubmit,
+      validate,
+      isValidInput,
+      onSubmitHandler,
+      id,
+      // inputState,
+      // requestStates, requestType,
+    } = this.props;
+    // const hasError = has(inputState, 'ModalEditor.syncErrors');
+    // const errorText = hasError ? inputState.ModalEditor.syncErrors.modalEditorInput.error : null;
+    // const canSend = requestStates[requestType] !== 'requested';
+    return (
+      <Modal isOpen fade={false} backdrop="static">
+        <ModalHeader>Add channel</ModalHeader>
+        <ModalBody>
+          <form onSubmit={handleSubmit(onSubmitHandler)} className="d-flex">
+            <Field name="modalInput" id={id} canSend validate={validate} component={InputField} />
+            <button type="submit" className="btn btn-primary" disabled={!isValidInput} >Add</button>
+            <Button color="secondary" onClick={handleToggleModal}>Cancel</Button>
+          </form>
+        </ModalBody>
+      </Modal>
+    );
+  }
+}
+
+export default ModalChannelNameEditor;
