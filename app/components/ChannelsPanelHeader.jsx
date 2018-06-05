54f0fc885546aba1aa4fc0bb922a1ac629bc27bf
UnnamedHero <hero.nospam@gmail.com>
2018-06-05 13:59:26 +0300

    channels panel header refactor

--- app/components/ChannelsPanelHeader.jsx
+++ app/components/ChannelsPanelHeader.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import cn from 'classnames';
 import { Button } from 'reactstrap';
 
-class ChannelsPanelHeader extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="flex-column">
-          <Button outline color="primary" size="sm">Toggle edit mode</Button>
-          <div className="flex-row">
-            Channels:{' '}
-            <Button className="primary" size="sm">+</Button>
-          </div>
+const ChannelsPanelHeader = (props) => {
+  const { editModeOn, handleToggleEditMode, handleToggleAddModal } = props;
+  const addButtonClasses = {
+    primary: true,
+    invisible: !editModeOn,
+  };
+  return (
+    <div>
+      <div className="flex-column">
+        <Button outline={!editModeOn} color="primary" size="sm" onClick={handleToggleEditMode}>
+          Toggle edit mode
+        </Button>
+        <div className="flex-row">
+          Channels:{' '}
+          <Button className={cn(addButtonClasses)} size="sm" onClick={handleToggleAddModal}>+</Button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ChannelsPanelHeader;
