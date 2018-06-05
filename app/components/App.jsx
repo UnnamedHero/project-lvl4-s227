4b6fc1100a1036eee3609d92fd2857f555b1690d
UnnamedHero <hero.nospam@gmail.com>
2018-06-05 13:53:15 +0300

    refactor classes for scrollable elements

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -11,7 +11,7 @@ import AlertNotifier from './AlertNotifier';
 const App = () => (
   <div className="container-fluid">
     <div className="row d-flex vh-100">
-      <div className="col-3 d-flex flex-column bg-dark text-white-50 vh-100">
+      <div className="col-3 bg-dark vh-100">
         <ChannelsPanel />
       </div>
       <div className="d-flex col-9 flex-column justify-content-between vh-100 bg-secondary">
