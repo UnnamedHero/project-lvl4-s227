fd96b0393793d3a628a8f8d984140911fc8be86f
UnnamedHero <hero.nospam@gmail.com>
2018-06-06 13:03:37 +0300

    remove state from NewMessageForm

--- app/components/NewMessageForm.jsx
+++ app/components/NewMessageForm.jsx
@@ -1,60 +1,27 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import cn from 'classnames';
-import connect from '../connect';
 import InputField from './InputField';
 
-const mapStateToProps = (state) => {
-  const props = {
-    currentChannelId: state.channels.currentChannelId,
-    userName: state.user.name,
-    sendsendMessageState: state.sendMessageState,
-    uiEditChannels: state.UI.editChannels,
-  };
-  return props;
-};
-
-@connect(mapStateToProps)
+@reduxForm({ form: 'NewMessage' })
 class NewMessageForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.el = React.createRef();
-  }
-
-  componentDidMount() {
-    // this.el.current.querySelector('input').focus();
-  }
-
-  componentDidUpdate(prevProps) {
-    const { sendingState: prevState } = prevProps;
-    const { sendingState: currState } = this.props;
-    if (prevState !== currState && currState === 'success') {
-      this.props.reset();
-      this.el.current.querySelector('input').focus();
-    }
-  }
-
   sendMessage = (values) => {
     this.props.sendMessage(values, this.props.currentChannelId, this.props.userName);
   }
 
   render() {
-    const { sendingState, uiEditChannels } = this.props;
-    const canSend = sendingState !== 'requested';
+    const { sendMessageHandler, sendMessageState } = this.props;
+    const requestPending = sendMessageState !== 'requested';
 
     const buttonClasses = {
-      btn: true,
-      'btn-primary': true,
-      disabled: !canSend,
+      disabled: requestPending,
     };
     return (
-      <form onSubmit={this.props.handleSubmit(this.sendMessage)} className="d-flex" ref={this.el}>
-        <Field name="messageText" component={InputField} canSend={canSend && !uiEditChannels} />
-        <button type="submit" className={cn(buttonClasses)} hidden>Send</button>
+      <form onSubmit={this.props.handleSubmit(sendMessageHandler)} className="d-flex">
+        <Field name="messageText" component={InputField} />
+        <button type="submit" hidden className={cn(buttonClasses)} />
       </form>);
   }
 }
 
-export default reduxForm({
-  form: 'newMessage',
-})(NewMessageForm);
+export default NewMessageForm;
