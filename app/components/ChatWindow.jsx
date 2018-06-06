19bd4616f8b1fe4361d9c716712892bad91cdd62
UnnamedHero <hero.nospam@gmail.com>
2018-06-06 13:03:17 +0300

    remove state from ChatWindow

--- app/components/ChatWindow.jsx
+++ app/components/ChatWindow.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import connect from '../connect';
-import { getCurrentChannelMessagesSelector } from '../selectors';
 
-const mapStateToProps = (state) => {
-  const props = {
-    messages: getCurrentChannelMessagesSelector(state),
-  };
-  return props;
-};
+class ChatWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.scrollTarget = React.createRef();
+  }
 
-const renderMessage = ({ id, author, body }) => (
-  <div className="list-group-item flex-column align-items-start w-100 bg-light rounded" key={id}>
-    <div className="d-flex w-100">
-      <p className="mb-1 font-weight-bold">{author}:</p>
-    </div>
-    <p className="mb-1">{body}</p>
-  </div>
-);
+  componentDidUpdate() {
+    this.scrollTarget.current.scrollIntoView({ behavior: 'smooth' });
+  }
 
-const ChatWindow = props => (
-  <div className="list-group">
-    {props.messages.map(renderMessage)}
-  </div>);
+  renderMessages() {
+    return this.props.messages.map(message => (
+      <div className="list-group-item flex-column align-items-start w-100 bg-light" key={message.id}>
+        <div className="d-flex w-100">
+          <p className="mb-1 font-weight-bold">{message.author}:</p>
+        </div>
+        <p className="mb-1">{message.body}</p>
+      </div>));
+  }
 
-
-export default connect(mapStateToProps)(ChatWindow);
+  render() {
+    return (
+      <div className="d-flex flex-column vh-100">
+        <div>
+          <h2>{this.props.userName}{' @ '}{this.props.currentChannelName}</h2>
+        </div>
+        <div className="list-group scrollable">
+          {this.renderMessages()}
+          <div ref={this.scrollTarget} />
+        </div>
+      </div>);
+  }
+}
+export default ChatWindow;
+// export default connect(mapStateToProps)(ChatWindow);
