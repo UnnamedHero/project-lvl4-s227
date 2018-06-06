1decd980a8633ccf71d5c2cb73d750e249e74bc5
UnnamedHero <hero.nospam@gmail.com>
2018-06-06 11:35:06 +0300

    add getCurrentChannelName selector

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -1,10 +1,18 @@
 import { createSelector } from 'reselect';
+import find from 'lodash/find';
 
 export const getMessages = state => state.messages;
 export const getCurrentChannel = state => state.channels.currentChannelId;
+export const getChannels = state => state.channels.channelsList;
 
 export const getCurrentChannelMessagesSelector = createSelector(
   getMessages,
   getCurrentChannel,
   (messages, currentChannelId) => messages.filter(m => m.channelId === currentChannelId),
+);
+
+export const getCurrentChannelName = createSelector(
+  getChannels,
+  getCurrentChannel,
+  (channels, currentChannelId) => find(channels, channel => channel.id === currentChannelId).name,
 );
