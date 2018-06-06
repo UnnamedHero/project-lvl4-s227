cfecaaa1fa412ead37bcfe039b50ce174868c00f
UnnamedHero <hero.nospam@gmail.com>
2018-06-06 13:03:03 +0300

    create ChatPanel component

--- app/components/ChatPanel.jsx
+++ app/components/ChatPanel.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import ChatHeader from './ChatHeader';
 import ChatWindow from './ChatWindow';
-import MessageInputBar from './MessageInputbar';
+import NewMessageForm from './NewMessageForm';
+import { getCurrentChannelMessagesSelector, getCurrentChannelName } from '../selectors';
+import connect from '../connect';
 
+const mapStateToProps = (state) => {
+  const props = {
+    currentChannelId: state.channels.currentChannelId,
+    messages: getCurrentChannelMessagesSelector(state),
+    currentChannelName: getCurrentChannelName(state),
+    userName: state.user.name,
+    sendMessageState: state.sendMessageState,
+  };
+  return props;
+};
+
+@connect(mapStateToProps)
 class ChatPanel extends React.Component {
+  sendMessage = (messageText) => {
+    this.props.sendMessage(messageText, this.props.currentChannelId, this.props.userName);
+  }
+
   render() {
+    const { userName, currentChannelName, messages } = this.props;
+
+    const chatWindowsProps = {
+      userName,
+      currentChannelName,
+      messages,
+    };
+
+    const newMessageFormProps = {
+      sendMessageHandler: this.sendMessage,
+      sendMessageState: this.props.sendMessageState,
+    };
+
     return (
-      <div className="d-flex col-9 flex-column justify-content-between vh-100 bg-secondary">
-        <ChatHeader />
-        <ChatWindow />
-        <MessageInputBar />
+      <div className="d-flex flex-column vh-100">
+        <ChatWindow {...chatWindowsProps} />
+        <NewMessageForm {...newMessageFormProps} />
       </div>);
   }
 }
-// <div className="scrollable word-wrap-bw">
+
 export default ChatPanel;
