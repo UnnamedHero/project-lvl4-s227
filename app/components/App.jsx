68b55964502d512f9ff65b8f3d84dc8e0a919205
UnnamedHero <hero.nospam@gmail.com>
2018-06-13 16:29:03 +0300

    add container padding

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -6,10 +6,10 @@ import ModalAlertNotifier from './ModalAlertNotifier';
 const App = () => (
   <div className="container-fluid px-0">
     <div className="d-flex vh-100">
-      <div className="col-3 bg-dark vh-100">
+      <div className="col-3 bg-dark vh-100 px-2">
         <ChannelsPanelContainer />
       </div>
-      <div className="d-flex col-9 flex-column vh-100 bg-secondary">
+      <div className="d-flex col-9 flex-column vh-100 bg-secondary px-2">
         <ChatPanelContainer />
       </div>
       <ModalAlertNotifier />
